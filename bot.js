7345c1953874f811ab2d283e2ed641215f081911
ajseward14-cell <ajseward14@gmail.com>
2026-01-31 16:54:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,6 @@ const bedrock = require('bedrock-protocol')
 const client = bedrock.createClient({
   host: 'server_ip',   
   port: 19132,         // optional, default 19132
-  username: 'Notch',   // the username you want to join as, optional if online mode
-  offline: true       // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
+  username: 'YOUR_MOM_BADDIE644',   // the username you want to join as, optional if online mode
+  offline: false       // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
 })
