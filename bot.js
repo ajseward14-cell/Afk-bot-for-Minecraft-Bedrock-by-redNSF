45d649e0952f88f8722bf799e1b7c5249f3ff54c
ajseward14-cell <ajseward14@gmail.com>
2026-01-31 17:25:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const bedrock = require('bedrock-protocol')
 const client = bedrock.createClient({
-  host: 'server_ip',   
-  port: 19132,         // optional, default 19132
-  username: 'YOUR_MOM_BADDIE644',   // the username you want to join as, optional if online mode
+  host: 'oneblock644.aternos.me',   
+  port: 46094,         // optional, default 19132
+  username: 'crusty toe67739',   // the username you want to join as, optional if online mode
   offline: false       // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
 })
